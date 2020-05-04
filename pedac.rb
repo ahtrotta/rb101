0fbcfae9a392ea2083445c2ab2ab060fb5d434c9
Adam Trotta <ahtrotta@gmail.com>
2020-05-04 10:52:27 -0400

    added pedac template and gitignore

---
+++ pedac.rb
@@ -0,0 +1,45 @@
+=begin
+
+*** Understand the Problem ***
+
+problem:
+
+input(s):
+
+output(s):
+
+problem domain (required knowledge):
+
+implicit requirement(s):
+
+clarifying question(s):
+
+mental model:
+
+
+=============================================================================
+
+*** Examples / Test Cases / Edge Cases ***
+
+provided example(s):
+
+test cases:
+
+
+=============================================================================
+
+*** Data Structure(s) ***
+
+data structure(s):
+
+
+=============================================================================
+
+*** Algorithm ***
+
+algorithm:
+
+
+=end
+
+
