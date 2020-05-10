0df7c44ae69ab75d6861845a4e30ef392c6f95b8
Adam Trotta <ahtrotta@gmail.com>
2020-05-10 15:40:12 -0400

    completed rps bonus feature 3

--- lesson2/rock_paper_scissors_lizard_spock.rb
+++ lesson2/rock_paper_scissors_lizard_spock.rb
@@ -27,18 +27,20 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+score = [0, 0]
+
 loop do
   choice = ''
   loop do
-    prompt("Choose one: (r)ock, (p)aper, s(c)issors, (l)izard, s(p)ock")
+    prompt("Choose one: (r)ock, (p)aper, (s)cissors, (l)izard, spoc(k)")
     choice = Kernel.gets().chomp()
     
     case choice
     when 'r'    then choice = 'rock'
     when 'p'    then choice = 'paper'
-    when 'c'    then choice = 'scissors'
+    when 's'    then choice = 'scissors'
     when 'l'    then choice = 'lizard'
-    when 'p'    then choice = 'spock'
+    when 'k'    then choice = 'spock'
     end
 
     if VALID_CHOICES.include?(choice)
@@ -54,6 +56,18 @@ loop do
 
   display_results(choice, computer_choice)
 
+  score[0] += 1 if win?(choice, computer_choice)
+  score[1] += 1 if win?(computer_choice, choice)
+
+  if score[0] == 5
+    prompt("You won 5 games! You are the grand winner!")
+    break
+  elsif score[1] == 5
+    prompt("The computer won 5 games! The computer is the grand winner!")
+    break
+  end
+
+  prompt("The score is you: #{score[0]}, computer: #{score[1]}")
   prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?('y')
