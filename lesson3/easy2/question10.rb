7fb374e52a35910c8d274c05c21c499ae93682cb
ahtrotta <ahtrotta@gmail.com>
2020-05-13 16:29:25 -0400

    completed easy2 exercises

---
+++ lesson3/easy2/question10.rb
@@ -0,0 +1,11 @@
+TABLE_WIDTH = 40
+
+title = "Flintstone Family Members"
+
+spaces = ( TABLE_WIDTH - title.length ) / 2
+
+puts ( ' ' * spaces ) + title
+puts title.center(40)
+
+# launch school solution
+# title.center(40)
