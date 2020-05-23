46c1320e768a2b4088f668e79e78c100e8b0b22a
Adam Trotta <ahtrotta@gmail.com>
2020-05-23 11:52:03 -0400

    progress on practice problems

---
+++ lesson5/practice_problems/practice_problem_8.rb
@@ -0,0 +1,8 @@
+hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'], fourth: ['over', 'the', 'lazy', 'dog']}
+
+hsh.each do |_, array|
+  array.each do |word|
+    puts word.delete('^[aeiou]').chars
+  end
+end
+
