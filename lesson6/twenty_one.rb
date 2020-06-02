23c1b145e2b5b7bafec616ac424740cb60fa0a5b
Adam Trotta <ahtrotta@gmail.com>
2020-06-01 23:42:38 -0400

    added dealers turn

--- lesson6/twenty_one.rb
+++ lesson6/twenty_one.rb
@@ -33,8 +33,7 @@ def get_value(hand)
 end
 
 def number_of_aces(hand)
-  card_names = hand.map { |card| card[:name] }
-  card_names.count('Ace')
+  hand.map { |card| card[:name] }.count('Ace')
 end
 
 def busted?(hand)
@@ -47,37 +46,63 @@ def busted?(hand)
   hand_value > 21
 end
 
-deck = initialize_deck.shuffle
-players_hand = deck.pop(2)
-dealers_hand = deck.pop(2)
+def game_over(outcome)
+  prompt "You #{outcome}! Play again? (y) or (n)"
+  answer = gets.chomp.downcase
+end
 
 loop do
-  player_cards = players_hand.map { |card| card[:name] }
-  visible_dealer_card = dealers_hand[0][:name]
+  deck = initialize_deck.shuffle
+  players_hand = deck.pop(2)
+  dealers_hand = deck.pop(2)
+  visible_dealer_cards = dealers_hand[0][:name]
 
   loop do
+    player_cards = players_hand.map { |card| card[:name] }
     prompt "Your hand: #{player_cards.join(', ')}"
-    prompt "Dealer's hand: #{visible_dealer_card} and unknown card"
+    prompt "Dealer's hand: #{visible_dealer_cards} and unknown card"
     prompt "(h)it or (s)tay?"
     answer = gets.chomp
     break if answer.downcase.start_with?('s')
     drawn_card = deck.pop(1)[0]
     prompt "You drew: #{drawn_card[:name]} of #{drawn_card[:suit]}"
+    players_hand << drawn_card
     break if busted?(players_hand)
   end
 
   if busted?(players_hand)
-    prompt "You lost! Play again? (y) or (n)"
-    response = gets.chomp
-    break if response.downcase.start_with?('n')
+    player_cards = players_hand.map { |card| card[:name] }
+    prompt "Your hand: #{player_cards.join(', ')}"
+    break if game_over('lost').start_with?('n')
   else
     prompt "You chose to stay! Dealer's turn."
   end
 
   loop do
     hand_value = get_value(dealers_hand)
-    if hand_value > 21
-    break if hand_value >= 17
+    aces = number_of_aces(dealers_hand)
+    while hand_value > 17 && aces > 0
+      hand_value -= 10
+      aces -= 1
+    end
+    if hand_value <= 17
+      dealer_card = deck.pop(1)[0]
+      prompt "Dealer chose to hit. Dealer drew #{dealer_card[:name]}."
+      dealers_hand << dealer_card
+    else
+      prompt "Dealer chose to stay."
+      break
+    end
+  end
+
+  if busted?(dealers_hand)
+    break if game_over('won').start_with?('n')
+  elsif get_value(dealers_hand) > get_value(players_hand)
+    break if game_over('lost').start_with?('n')
+  elsif get_value(dealers_hand) < get_value(players_hand)
+    break if game_over('won').start_with?('n')
+  else
+    break if game_over('tied').start_with?('n')
   end
 
 end
